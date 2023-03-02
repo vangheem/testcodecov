39e08baf1fbe263c4f5adb2e9a461f09240390a3
vangheem <vangheem@gmail.com>
2023-03-02 17:39:43 -0500

    try

---
+++ setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+
+setup(
+    name="testcodecov",
+    version="0.1.0",
+    zip_safe=True,
+    include_package_data=True,
+    packages=find_packages(),
+)
